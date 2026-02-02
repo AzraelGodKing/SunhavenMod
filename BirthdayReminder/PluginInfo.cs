c90857e63f0d9f18354445ad66453724648f7dcf
AzraelGodKing <AzraelGodsKing@gmail.com>
2026-02-01 20:30:22 -0500

    Adding Link to nexus page

--- BirthdayReminder/PluginInfo.cs
+++ BirthdayReminder/PluginInfo.cs
@@ -3,7 +3,7 @@ namespace BirthdayReminder
     public static class PluginInfo
     {
         public const string PLUGIN_GUID = "com.azraelgodking.squirrelsbirthdayreminder";
-        public const string PLUGIN_NAME = "Squirrels Birthday Reminder";
+        public const string PLUGIN_NAME = "A Squirrel's Birthday Reminder";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
