134f3b8c76be4d0cf7213ccdacb2b2307fdd5c09
AzraelGodKing <AzraelGodsKing@gmail.com>
2026-03-27 17:16:44 -0400

    Bumping to 3.0.1

--- TheVault/Plugin.cs
+++ TheVault/Plugin.cs
@@ -1197,7 +1197,7 @@ namespace TheVault
     {
         public const string PLUGIN_GUID = "com.azraelgodking.thevault";
         public const string PLUGIN_NAME = "The Vault";
-        public const string PLUGIN_VERSION = "3.0.0";
+        public const string PLUGIN_VERSION = "3.0.1";
     }
 
     /// <summary>
